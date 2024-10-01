a46c7455f01e621b8a19451d81228178326c973d
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-01 17:57:32 +0100

    all

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => { console.log('Got a response from the API'); })
-    .catch(() => { Error(); });
+    // .then(() => { console.log('Got a response from the API'); })
+    .catch(() => { Error(); })
+    .finally(() => { console.log('Got a response from the API'); });
 }
