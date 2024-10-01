19f2027d8d676f178855a990db11db05cc14c5e8
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-01 18:20:42 +0100

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  Promise.all([promise1, promise2])
+    .then(([body, firstLastname]) => {
+      console.log(`${body.body} ${firstLastname.firstName} ${firstLastname.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+export default handleProfileSignup;
