9e6c919d96cc6a3e9b6d3f4d1a7efa52525b1cbd
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-03 19:37:32 +0100

    all

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,7 +4,23 @@ export default class Airport {
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
   get [Symbol.toStringTag]() {
-    return `Airport [SFO] { _name: ${this._name}, _code: ${this._code} }`;
+    return this._code;
   }
 }
