f79163026364208684231137ee56bd70e827eba4
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-03 19:28:17 +0100

    all

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,13 +1,17 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(floors, sqft) {
+  constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
   get floors() {
     return this._floors;
+  }
+
+  set floors(newFloor) {
+    this._floors = newFloor;
   }
 
   evacuationWarningMessage() {
