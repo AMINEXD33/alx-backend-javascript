b09ee61b16f8bf7113f1c0283a847189ee34ea82
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-08 19:43:58 +0100

    all

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,6 @@ export default function getStudentIdsSum(array) {
   if (!Array.isArray(array)) {
     return 0;
   }
-  const oldsum = 0;
-  const sum = array.reduce((prev, current) => prev.id + current.id);
+  const sum = array.reduce((prev, current) => prev + current.id, 0);
   return sum;
 }
