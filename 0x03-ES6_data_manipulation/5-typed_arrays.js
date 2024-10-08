23aff7a3e7689cb1120c7e9cb571787eca156dd7
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-08 23:39:01 +0100

    all

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,6 +3,9 @@ export default function createInt8TypedArray(
   position,
   value,
 ) {
+  if (length == 0) {
+    return [];
+  }
   if (position >= length) {
     throw new Error('Position outside range');
   }
