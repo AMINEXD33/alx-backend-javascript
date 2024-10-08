de80b7373b0e11c9eacf5d03d21afb3fda0daaa8
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-08 20:06:10 +0100

    all

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,29 @@
+export default function updateStudentGradeByCity(
+  array,
+  city,
+  newGrades,
+) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  const newList = [];
+  array.forEach((student) => {
+    const studentId = student.id;
+    const city_ = student.location;
+    const dummyObj = student;
+    if (city_ === city) {
+      let actionPerformed = false;
+      newGrades.forEach((grade) => {
+        if (grade.studentId === studentId) {
+          dummyObj.grade = grade.grade;
+          actionPerformed = true;
+        }
+      });
+      if (!actionPerformed) {
+        dummyObj.grade = 'N/A';
+      }
+    }
+    newList.push(dummyObj);
+  });
+  return newList;
+}
