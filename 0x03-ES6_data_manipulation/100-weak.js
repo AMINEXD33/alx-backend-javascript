f1a5a2a008480f006bc49f141ad87ec1288cfb42
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-09 02:30:09 +0100

    all

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,14 +2,13 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(obj) {
   if (weakMap.has(obj)) {
-    let oldval = weakMap.get(obj);
+    const oldval = weakMap.get(obj);
     weakMap.set(obj, oldval + 1);
-  }
-  else {
+  } else {
     weakMap.set(obj, 1);
   }
 
   if (weakMap.get(obj) >= 5) {
-    throw new Error("Endpoint load is high");
+    throw new Error('Endpoint load is high');
   }
 }
