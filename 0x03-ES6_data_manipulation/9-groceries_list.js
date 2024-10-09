49ad45668b2dffdd67a54955ad0bd9558f2c2b86
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-09 01:57:33 +0100

    all

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const arr = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
+
+  const map = new Map();
+  for (const element of arr) {
+    map.set(element[0], element[1]);
+  }
+  return map;
+}
