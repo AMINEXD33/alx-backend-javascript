fa760a8b6123c93a05f9a913b8771fcc1d66a7b3
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-09 01:37:13 +0100

    all

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
-export default function cleanSet(set, word) {
+export default function cleanSet(set, startString) {
   let result = null;
-  if (word.trim() === '') {
+  if (startString.trim() === '') {
     return '';
   }
   for (const element of set) {
-    if (element.startsWith(word)) {
-      const subword = element.split(word.charAt(word.length - 1), 2);
-      if (result === null && subword[1] !== null) {
+    if (element.startsWith(startString)) {
+      const substartString = element.split(startString.charAt(startString.length - 1), 2);
+      if (result === null && substartString[1] !== null) {
         result = '';
-        result += subword[1];
-      } else if (subword[1] !== null) {
-        result += `-${subword[1]}`;
+        result += substartString[1];
+      } else if (substartString[1] !== null) {
+        result += `-${substartString[1]}`;
       }
     }
   }
