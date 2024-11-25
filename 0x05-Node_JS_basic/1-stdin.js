4344fa087a0d470d6a14cdb2c754e029641589ca
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-25 17:30:57 +0100

    task 0

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,16 @@
-const message = "Welcome to Holberton School, what is your name?\n";
-process.stdout.write(message);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
-  process.exit();
-});
-
-process.on("exit", () => {
-  process.stdout.write("This important software is now closing\n");
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
