f48198e0890ed8e0b1f2e7d35cb6814b54827f39
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-25 17:19:30 +0100

    task 0

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("data", function (data) {
-  process.stdout.write("Your name is: " + data);
-  process.exit(1);
+  process.stdout.write("Your name is: " + data.toString());
+  process.exit();
 });
 process.on("exit", () => {
   process.stdout.write("This important software is now closing\n");
