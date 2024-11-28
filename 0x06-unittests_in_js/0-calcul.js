52462cc5f027b3e184313c1c73961de3bef10c58
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-28 23:48:13 +0100

    task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,3 @@
-function calculateNumber(a, b) {
-  const number1 = Number(a);
-  const number2 = Number(b);
-  if (Number.isNaN(number1) || Number.isNaN(number2)) {
-    throw new Error("One of the args are not an int and can't be converted");
-  }
-  return Math.round(number1) + Math.round(number2);
-}
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
